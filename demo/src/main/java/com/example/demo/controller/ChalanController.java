a27d641a3b1556cd268f9c8d8d6aa4dda036ccf6
godhanismit <godhanismit45@gmail.com>
2025-12-15 17:49:43 +0530

    Some Changes!

--- demo/src/main/java/com/example/demo/controller/ChalanController.java
+++ demo/src/main/java/com/example/demo/controller/ChalanController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/chalans")
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "*")
 public class ChalanController {
     private final ChalanService service;
 
--- my-project/src/api.js
+++ my-project/src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API = axios.create({
   // eslint-disable-next-line no-undef
-  baseURL: 'https://embroidery-gamma.vercel.app',
+  baseURL: 'http://localhost:8080/api',
 });
 
 export default API;
